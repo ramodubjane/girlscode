9e93f9bd3e5ddf46fad15862151b6c20d2f826db
ramodubjane <143597409+ramodubjane@users.noreply.github.com>
2024-08-12 11:25:51 +0200

    changes

---
+++ # Streamlit dependencies.py
@@ -0,0 +1,14 @@
+# Streamlit dependencies
+import streamlit as st
+
+# Data handling dependencies
+import pandas as pd
+import numpy as np
+# App declaration
+def main():
+
+    # DO NOT REMOVE the 'Recommender System' option below,  however, 
+    # you are welcome to add more options to enrich your app.
+    page_options = ["About GirlsCode", "Contact Us", "FAQ"]
+if __name__ == '__main__':
+    main()
